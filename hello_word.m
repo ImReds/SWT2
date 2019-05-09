7685ea5777aeeb428d3577240b690fb985feb14d
Andrea <andrea.rossi@xtreamers.io>
2019-05-09 15:22:13 +0200

    maybe one turn function is good

--- hello_word.m
+++ hello_word.m
@@ -1,56 +1,89 @@
-%{
-function tilt = average(x)
-if ~isvector(x)
-    error('Input must be a vector')
-end
-y = sum(x)/length(x); 
-end
-
-%}
+clear all;
 myrobot = legoev3('usb');
+
+whiteThreshold = 12;
 
 myUltrasonicSensor = sonicSensor(myrobot);
 val = readDistance(myUltrasonicSensor);
 
-ColorSensorRight = colorSensor(myrobot, 3);
-ColorSensorLeft = colorSensor(myrobot, 2);
+ ColorSensorRight = colorSensor(myrobot, 3);
+ ColorSensorLeft = colorSensor(myrobot, 2);
 
 % left is motor A, right is motor B
+global motorLeft; 
 motorLeft = motor(myrobot, 'A');
+global motorRight;
 motorRight = motor(myrobot, 'B');
 
 motorLeft.Speed = 25;
 motorRight.Speed = 25;
-
 start(motorLeft);
 start(motorRight);
+
 colorRight = readLightIntensity(ColorSensorRight);
 colorLeft = readLightIntensity(ColorSensorLeft);
 
-while(colorRight >7) && (colorLeft >7)
-    val = readDistance(myUltrasonicSensor);    
+while( (colorRight >whiteThreshold) || (colorLeft >whiteThreshold) )
+    motorLeft.Speed = 25;
+    motorRight.Speed = 25;
+    start(motorLeft);
+    start(motorRight);
+    
+    fprintf('colorLeft: %d colorRight: %d\n', colorLeft, colorRight);
+    
+    if(colorRight <whiteThreshold)
+        %turn right
+        break;
+    end
+    
+    if(colorLeft <whiteThreshold)
+        turnLeft(motorLeft, motorRight, ColorSensorLeft, ColorSensorRight, whiteThreshold)
+        %turn left
+        break;
+    end
+    
+    %val = readDistance(myUltrasonicSensor);    
     colorRight = readLightIntensity(ColorSensorRight);
     colorLeft = readLightIntensity(ColorSensorLeft);
-    display(colorRight);
 end
+
+% stop function
 
 stop(motorLeft, 1);
 stop(motorRight, 1);
-
-
-
-%{
-
-while val<2
-
-    val = readDistance(myUltrasonicSensor);    
-    ambient = readLightIntensity(myColourSensor);
-    display(ambient);
-    
-    %reflected = readLightIntensity(myColourSensor, 'reflected');
-    %display(reflected);
-    %display(val);
-end
-%}
-
 clear all;
+
+function [] = turnLeft( motorLeft, motorRight, ColorSensorLeft, ColorSensorRight, whiteThreshold)
+    disp('i am turn');
+    motorLeft.Speed = -25;
+    motorRight.Speed = 25;
+    start(motorLeft);
+    start(motorRight);
+    
+    while true
+        colorRight = readLightIntensity(ColorSensorRight);
+        colorLeft = readLightIntensity(ColorSensorLeft);
+        
+        if(colorRight< whiteThreshold) 
+            break;
+        end
+    end
+    
+    fprintf('inside turn: colorLeft: %d colorRight: %d\n', colorLeft, colorRight);
+    %{
+    pause( 0.4 );
+    stop(motorLeft, 1);
+    stop(motorRight, 1);
+    %}
+end
+
+function [] = backUp( motorLeft, motorRight)
+    disp('i am backing');
+    motorLeft.Speed = -25;
+    motorRight.Speed = -25;
+    start(motorLeft);
+    start(motorRight);
+    pause( 0.25 );
+    stop(motorLeft, 1);
+    stop(motorRight, 1);
+end
